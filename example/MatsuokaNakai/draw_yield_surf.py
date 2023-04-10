588d6eb0b88fe2b621de74f80ba59254cf030573
Hyoung Suk Suh <hyoungsuksuh@hyoungsuksuhs-MacBook-Pro.local>
2023-04-10 12:04:43 -0400

    add NAM func for Matsuoka-Nakai

--- example/MatsuokaNakai/draw_yield_surf.py
+++ example/MatsuokaNakai/draw_yield_surf.py
@@ -23,6 +23,10 @@ from example.MatsuokaNakai.f_benchmark import *
 get_dfdrho = egrad(f_benchmark, 1)
 
 
+# NAM yield function
+from example.MatsuokaNakai.f_NAM import *
+
+
 # QNM yield function
 from example.MatsuokaNakai.f_QNM import *
 
@@ -97,6 +101,75 @@ for i in range(np.shape(theta)[0]):
 
     if err < tol:
       rho3[i] = x
+      break
+# -----------------------------------------------------------------
+
+# -----------------------------------------------------------------
+# Return mapping for NAM yield function
+rho_NAM1 = np.zeros_like(theta)
+for i in range(np.shape(theta)[0]):
+
+  x = p_spec1
+
+  print(">> Point", i, "------------------------------------")
+
+  for ii in range(maxiter):
+    res = f_NAM(p_spec1, x, theta[i])
+    jac = 1
+    
+    dx = -res / jac
+    x = x + dx
+
+    err = np.linalg.norm(dx)
+
+    print(" Newton iter.",ii, ": err =", err)
+
+    if err < tol or ii == maxiter-1:
+      rho_NAM1[i] = x
+      break
+
+rho_NAM2 = np.zeros_like(theta)
+for i in range(np.shape(theta)[0]):
+
+  x = p_spec2
+
+  print(">> Point", i, "------------------------------------")
+
+  for ii in range(maxiter):
+    res = f_NAM(p_spec2, x, theta[i])
+    jac = 1
+    
+    dx = -res / jac
+    x = x + dx
+
+    err = np.linalg.norm(dx)
+
+    print(" Newton iter.",ii, ": err =", err)
+
+    if err < tol or ii == maxiter-1:
+      rho_NAM2[i] = x
+      break
+
+rho_NAM3 = np.zeros_like(theta)
+for i in range(np.shape(theta)[0]):
+
+  x = p_spec3
+
+  print(">> Point", i, "------------------------------------")
+
+  for ii in range(maxiter):
+    res = f_NAM(p_spec3, x, theta[i])
+    jac = 1
+    
+    dx = -res / jac
+    x = x + dx
+
+    err = np.linalg.norm(dx)
+
+    print(" Newton iter.",ii, ": err =", err)
+
+    if err < tol or ii == maxiter-1:
+      rho_NAM3[i] = x
       break
 # -----------------------------------------------------------------
 
@@ -245,6 +318,9 @@ ax = fig.add_subplot(111, projection='polar')
 ax.plot(theta, rho1, 'k-', linewidth=2.0, label='Analytical')
 ax.plot(theta, rho2, 'k-', linewidth=2.0)
 ax.plot(theta, rho3, 'k-', linewidth=2.0)
+ax.plot(theta, rho_NAM1, 'g--', linewidth=1.5, label='NAM')
+ax.plot(theta, rho_NAM2, 'g--', linewidth=1.5)
+ax.plot(theta, rho_NAM3, 'g--', linewidth=1.5)
 ax.plot(theta, rho_QNM1, 'r--', linewidth=1.5, label='QNM')
 ax.plot(theta, rho_QNM2, 'r--', linewidth=1.5)
 ax.plot(theta, rho_QNM3, 'r--', linewidth=1.5)
---
+++ example/MatsuokaNakai/f_NAM.py
@@ -0,0 +1,20 @@
+import torch
+import joblib
+import numpy as np
+
+model_NAM = torch.jit.load("example/MatsuokaNakai/step_1_nn_training/results_21/cylindrical/baseLO/PosEnc_0/model.ptjit")
+f_INPUT_scaler  = joblib.load("example/MatsuokaNakai/step_1_nn_training/results_21/cylindrical/baseLO/PosEnc_0/xscaler.joblib")
+f_OUTPUT_scaler = joblib.load("example/MatsuokaNakai/step_1_nn_training/results_21/cylindrical/baseLO/PosEnc_0/yscaler.joblib")
+
+def f_NAM(p, rho, theta):
+
+    RT = np.array([p, rho, theta]).reshape(1,3)
+    RT = f_INPUT_scaler.transform(RT)
+    RT = torch.tensor(RT, dtype=torch.float)
+    
+    f = model_NAM(RT)
+    f = f[0]
+    f_numpy = f.detach().numpy()[0,0]
+    f_numpy = f_OUTPUT_scaler.inverse_transform(f_numpy.reshape(-1,1))
+
+    return f_numpy[0,0]
